230f63f31624193c489db72505cd057ea74d6523
ulkoart <ulkoart@gmail.com>
2020-04-19 12:32:17 +0300

    Initial Commit

---
+++ Covid19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid19
+//
+//  Created by user on 19.04.2020.
+//  Copyright © 2020 ulkoart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
